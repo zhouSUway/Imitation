5c06c3e8b8c2ea51455f88f0192110f3f721d82f
zhouSUway <1587850807@qq.com>
2018-12-08 22:07:33 +0800

    pageviewmapper

--- src/main/java/com/qianfeng/analystic/mr/pv/PageViewOutputWriter.java
+++ src/main/java/com/qianfeng/analystic/mr/pv/PageViewOutputWriter.java
@@ -41,7 +41,5 @@ public class PageViewOutputWriter implements IOutputWriter {
 
         //添加到bacth
         pst.addBatch();
-
-
     }
 }
--- src/main/java/com/qianfeng/analystic/mr/pv/PageViewReducer.java
+++ src/main/java/com/qianfeng/analystic/mr/pv/PageViewReducer.java
@@ -1,20 +1,44 @@
 package com.qianfeng.analystic.mr.pv;
 
+
 import com.qianfeng.analystic.model.dim.StatsUserDimension;
-import com.qianfeng.analystic.model.dim.base.BaseDimension;
-import com.qianfeng.analystic.model.dim.value.BaseOutputValueWritable;
-import com.qianfeng.analystic.mr.IOutputWriter;
-import com.qianfeng.analystic.mr.service.IDimensionConvert;
-import org.apache.hadoop.conf.Configuration;
+import com.qianfeng.analystic.model.dim.base.KpiDimension;
+import com.qianfeng.analystic.model.dim.value.map.TimeOutputValue;
+import com.qianfeng.analystic.model.dim.value.reduce.MapperOutputValue;
+import com.qianfeng.common.KpiTypeEnum;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.MapWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
 
 import java.io.IOException;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
 
-public class PageViewReducer implements IOutputWriter {
+
+public class PageViewReducer extends Reducer<StatsUserDimension,TimeOutputValue,StatsUserDimension,MapperOutputValue> {
+
+    private IntWritable one = new IntWritable(-1);
+    private MapperOutputValue mov = new MapperOutputValue();
+
+
     @Override
-    public void outputWrite(Configuration conf, BaseDimension key, BaseOutputValueWritable value, PreparedStatement ps, IDimensionConvert convert) throws IOException, SQLException {
-        StatsUserDimension statsUserDimension = (StatsUserDimension) key;
+    protected void reduce(StatsUserDimension key, Iterable<TimeOutputValue> values, Context context) throws IOException, InterruptedException {
+        //map循环过来的value
+        int count = 0;
+        for (TimeOutputValue timeOutputValue:values){
+
+            //将uuid取出来进行累计
+            count++;
+        }
+
+        //构建输出的value
+
+        MapWritable mapWritable = new MapWritable();
+        mapWritable.put(one,new IntWritable(count));
+        this.mov.setMapWritable(mapWritable);
+
+        this.mov.setKpiTypeEnum(KpiTypeEnum.valueOfType(key.getStatsCommonDimension().getKpiDimension().getKpiName()));
+
+        context.write(key,this.mov);
 
     }
 }
