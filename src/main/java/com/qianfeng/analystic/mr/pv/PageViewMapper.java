6ee0c6550ff8d0c9dcb28c80b1c8feaee96c9bdc
zhouSUway <1587850807@qq.com>
2018-12-08 21:51:09 +0800

    pageviewmapper

--- src/main/java/com/qianfeng/analystic/mr/pv/PageViewMapper.java
+++ src/main/java/com/qianfeng/analystic/mr/pv/PageViewMapper.java
@@ -1,8 +1,13 @@
 package com.qianfeng.analystic.mr.pv;
 
+import com.qianfeng.analystic.model.dim.StatsCommonDimension;
 import com.qianfeng.analystic.model.dim.StatsUserDimension;
+import com.qianfeng.analystic.model.dim.base.BrowserDimension;
+import com.qianfeng.analystic.model.dim.base.DateDimension;
 import com.qianfeng.analystic.model.dim.base.KpiDimension;
+import com.qianfeng.analystic.model.dim.base.PlatformDimension;
 import com.qianfeng.analystic.model.dim.value.map.TimeOutputValue;
+import com.qianfeng.common.DateEnum;
 import com.qianfeng.common.EventConstant;
 import com.qianfeng.common.KpiTypeEnum;
 import com.qianfeng.util.LogUtil;
@@ -15,6 +20,7 @@ import org.apache.hadoop.hbase.util.Bytes;
 import org.apache.log4j.Logger;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Map;
 
 
@@ -47,6 +53,34 @@ public class PageViewMapper extends TableMapper<StatsUserDimension,TimeOutputVal
         //构建输出value
         Long serviceTimes = Long.valueOf(serviceTime);
 
+        this.vs.setId(url);
+        this.vs.setTime(serviceTimes);
+
+//        构建输出的类
+
+        List<PlatformDimension> platformDimensions = PlatformDimension.buildPlatform(platition);
+        DateDimension dateDimension = DateDimension.buildDate(serviceTimes,DateEnum.DAY);
+        List<BrowserDimension> browserDimensions = BrowserDimension.buildBrower(browerName, browerVerion);
+
+        StatsCommonDimension statsCommonDimension = this.ks.getStatsCommonDimension();
+//        为StatsCommonDimension赋值
+        statsCommonDimension.getDateDimension(dateDimension);
+
+        BrowserDimension browserDimension = new BrowserDimension("", "");
+
+        //循环平台维度集合对象
+
+        for (PlatformDimension pl :platformDimensions){
+            statsCommonDimension.setPlatformDimension(pl);
+            //循环输出的用于浏览器模块新增用户的指标
+            for (BrowserDimension br:browserDimensions){
+                statsCommonDimension.setKpiDimension(PageViewKpi);
+                this.ks.setStatsCommonDimension(statsCommonDimension);
+                this.ks.setBrowserDimension(br);
+                //进行输出
+                context.write(this.ks,this.vs);
+            }
+        }
 
 
     }
